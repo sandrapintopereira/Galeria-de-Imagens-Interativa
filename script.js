c4de6867ace2d60ab342b2f8963a46ebe1e95968
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-02-05 12:04:38 +0000

    imagem só fecha quando clicado no x ou fora da imagem

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const parede = document.querySelector(".parede");
 const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
 const modalImg = document.querySelector(".galeria-imagem");
 const fechar = document.querySelector(".fechar");
 
@@ -13,3 +14,16 @@ parede.addEventListener("click", (e) => {
   modal.classList.add("ativo");
 });
 
+
+fechar.addEventListener("click", (e) => {
+    e.stopPropagation();
+    modal.classList.remove("ativo");
+});
+
+modalContent.addEventListener("click", (e) => {
+    e.stopPropagation();
+});
+
+modal.addEventListener("click", () => {
+    modal.classList.remove("ativo");
+})
